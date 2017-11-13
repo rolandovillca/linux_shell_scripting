b48ad798b8c10592937f7c831bd7a374dc192d6b
rolandovillca <rolandovillca@gmail.com>
2017-11-13 17:40:21 -0500

    Fix for statement and renaming modules

--- colors/color_sample_1.sh
+++ colors/color_sample_1.sh
@@ -22,13 +22,3 @@ do
     echo -e "\e[0;${i}m Hello world!!!"
 done
 echo
-
-# EXAMPLE 3:
-# =========
-for code in {0..255}; do echo -e "\e[38;05;${code}m $code: Test"; done
-echo
-
-# EXAMPLE 4:
-# =========
-i=30; while true;do echo -e "$i = \e[0;$[i++]mStackOverFlow\e[m";if [ $i == 49 ];then break;fi; sleep 1;done
-echo
