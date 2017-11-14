362afedbc823994ae4d462d980cf9610bb26382f
Rolando Villca <rolandovillca@gmail.com>
2017-11-14 07:07:05 -0500

    Adding assignment and constants examples

---
+++ basics/substitutions_and_constants.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ASSIGNMENTS:
+# ===========
+# Assign the result of a command to a variable:
+right_now=$(date +"%x %r %Z")
+
+# Nesting variable:
+time_stamp="Updated on $right_now by $USER"
+
+
+# CONSTANTS:
+# =========
+TITLE="Learning constants for $HOSTNAME"
+RIGHT_NOW=$(date +"%x %r %Z")
+TIME_STAMP="Updated on $RIGHT_NOW by $USER"
+
+echo "Title: $TITLE"
+echo "Timestamp: $TIME_STAMP"
