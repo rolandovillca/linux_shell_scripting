3f42753a2243ff390fd0741bb4517a65f8baf573
rolandovillca <rolandovillca@gmail.com>
2017-11-14 16:42:15 -0500

    Adding a second color example handling color code

--- colors/color_sample_2.sh
+++ colors/color_sample_2.sh
@@ -1,1 +1,9 @@
+# EXAMPLE 3:
+# =========
+for code in {0..255}; do echo -e "\e[38;05;${code}m $code: Test"; done
+echo
 
+# EXAMPLE 4:
+# =========
+i=30; while true;do echo -e "$i = \e[0;$[i++]mStackOverFlow\e[m";if [ $i == 49 ];then break;fi; sleep 1;done
+echo
