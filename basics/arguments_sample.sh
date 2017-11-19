119c720b77124783f7d4c5d5cc46018ed2d1dce8
rolandovillca <rolandovillca@gmail.com>
2017-11-19 09:59:15 -0400

    Rename files and update excericies

--- basics/arguments_sample.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Total of arguments: $#"
-echo "Name of script: $0"
-echo "$1"
-echo "$2"
---
+++ basics/command_line_arguments.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Total of arguments: $#"
+echo "Name of script: $0"
+echo "$1"
+echo "$2"
+
+# How to check if I have at least one parameter:
+
+# The first way:
+if [ "$1" != "" ]; then
+    echo "Positional parameter 1 contains something"
+else
+    echo "Positional parameter 1 is empty"
+fi
+
+# The second way:
+if [ $# -gt 0 ]; then
+    echo "Your command line contains $# arguments"
+else
+    echo "Your command line contains no arguments"
+fi
